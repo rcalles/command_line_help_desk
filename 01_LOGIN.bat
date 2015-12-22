cd7b5bd113a4082786180b0ab44be629d55aea9b
Ricardo <ricardo.cufm@gmail.com>
2015-12-22 17:21:27 +0000

    First steps

--- 01_LOGIN.bat
+++ 01_LOGIN.bat
@@ -5,7 +5,6 @@ set lists=%root%Lists\
 set Helpdeskers=%Lists%Helpdeskers.txt
 set Settings=%lists%Settings.txt
 
-rem this should come from settings
 setlocal EnableDelayedExpansion
 
 for /f %%a in (%Settings%) do (
@@ -32,16 +31,13 @@ rem for /l %%x in (1,1,%counter%) do @(
 rem 	echo !List[%%x]!
 rem 	)
 
-
-REM This batch asks for Domain-controller credentials in order to perform AD operations
-REM then opens the SECONDARY.bat
-
 echo[
 
-set /p superuser=	Select your Domain-controller user: 
+set /p superuser=		Select your Domain-controller user: 
+
+if %superuser%==. exit
 
 runas /user:!List[%superuser%]!@%domain% "%root%02_MAIN.bat" 
 
-if %superuser%==. exit
 
 exit
